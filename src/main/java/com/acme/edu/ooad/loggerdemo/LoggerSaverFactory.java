7023d28c3d3c8d46a0cd883fea3743bf9331bf0a
vbutrim <vbutrim@gmail.com>
2017-08-30 13:28:08 +0300

    delete Eugenue's code

--- src/main/java/com/acme/edu/ooad/loggerdemo/LoggerSaverFactory.java
+++
@@ -1,7 +0,0 @@
-package com.acme.edu.ooad.loggerdemo;
-
-public class LoggerSaverFactory { //XML, JSON, DB, USER
-    public static LoggerSaver create() {
-        return new ConsoleSaver();
-    }
-}
